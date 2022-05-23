6f708aa5b15930572151c3ea41e30af698ece6ca
Phil Martin <phil.martin1983@gmail.com>
2022-05-23 23:31:39 +0100

    Added some integration tests.

--- AnimalCrossingFunTests/AnimalCrossingFunTests.swift
+++ AnimalCrossingFunTests/AnimalCrossingFunTests.swift
@@ -8,29 +8,41 @@
 import XCTest
 @testable import AnimalCrossingFun
 
-class AnimalCrossingFunTests: XCTestCase {
-
+class AnimalCrossingFunTests: XCTestCase, ACCritterListViewModelOutput {
+    
+    var expectation: XCTestExpectation?
+    var critterResponse: CritterResponseModel?
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        critterResponse = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testAPIIntegration() {
+        let viewModel = ACCritterListViewModel()
+        viewModel.output = self
+        viewModel.fetch(critterType: ACCritterType.fish)
+        expectation = expectation(description: "Fetch Critter")
+        waitForExpectations(timeout: 1)
+        do {
+            let result = try XCTUnwrap(critterResponse)
+            XCTAssertEqual(result.name.nameEUen, "bitterling")
+        } catch {
+            XCTFail("Do catch failed")
         }
     }
-
+    
+    func contentRecieved(model: [CritterResponseModel]) {
+        critterResponse = model.first
+        expectation?.fulfill()
+        expectation = nil
+    }
+    
+    func requestFailed(error: String) {
+        
+    }
 }
