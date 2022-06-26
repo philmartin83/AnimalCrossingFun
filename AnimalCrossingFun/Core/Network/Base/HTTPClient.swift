84cd10d2a8d3da02db5c32042168ac22e5b4d283
Phil Martin <phil.martin1983@gmail.com>
2022-06-26 19:40:09 +0100

    removed debug code

--- AnimalCrossingFun/Core/Network/Base/HTTPClient.swift
+++ AnimalCrossingFun/Core/Network/Base/HTTPClient.swift
@@ -36,18 +36,6 @@ extension HTTPClient {
                 do {
                     let decodedResponse = try JSONDecoder().decode(responseModel, from: data)
                     return .success(decodedResponse)
-                } catch DecodingError.keyNotFound(let key, let context) {
-                    Swift.print("could not find key \(key) in JSON: \(context.debugDescription)")
-                    return .failure(.decode)
-                } catch DecodingError.valueNotFound(let type, let context) {
-                    Swift.print("could not find type \(type) in JSON: \(context.debugDescription)")
-                    return .failure(.decode)
-                } catch DecodingError.typeMismatch(let type, let context) {
-                    Swift.print("type mismatch for type \(type) in JSON: \(context.debugDescription)")
-                    return .failure(.decode)
-                } catch DecodingError.dataCorrupted(let context) {
-                    Swift.print("data found to be corrupted in JSON: \(context.debugDescription)")
-                    return .failure(.decode)
                 } catch {
                     return .failure(.decode)
                 }
